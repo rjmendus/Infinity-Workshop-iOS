1179976eca0b069ecf02dc5107012a997a23bff8
Rajat Jaic Mendus <jaicmendus@gmail.com>
2018-11-16 08:25:28 +0530

    Add view orders table view

--- Bootcamp/Controller/ViewOrdersViewController.swift
+++ Bootcamp/Controller/ViewOrdersViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewOrdersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     // Data model: These strings will be the data for the table view cells
-    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    var orders: [String] = []
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
@@ -22,6 +24,9 @@ class ViewOrdersViewController: UIViewController, UITableViewDelegate, UITableVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get the orders from web
+        self.getOrders()
         
         // Register the table view cell class and its reuse id
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
@@ -34,19 +39,36 @@ class ViewOrdersViewController: UIViewController, UITableViewDelegate, UITableVi
         tableView.dataSource = self
     }
     
+    func getOrders() {
+        let url = "https://9nfmj2dq1f.execute-api.ap-south-1.amazonaws.com/Development/orders/get-all"
+        
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                self.orders = json["Menu_ITEMS"].arrayValue.map({$0["OrderDate"].stringValue})
+                print(self.orders)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.animals.count
+        return self.orders.count
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // create a new cell if needed or reuse an old one
-        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! UITableViewCell
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
         
         // set the text from the data model
-        cell.textLabel?.text = self.animals[indexPath.row]
+        cell.textLabel?.text = self.orders[indexPath.row]
         
         return cell
     }
